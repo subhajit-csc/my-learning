890368da27c0f8d7255c6ec943fd010afb3a6654
Subhajit Manna <subhajit-csc@users.noreply.github.com>
2022-05-26 17:53:59 +0530

    mock server added
    mock server added

---
+++ src/server/routes/users.js
@@ -0,0 +1,69 @@
+
+const getUserData = (app, fs) => {
+
+    // variables
+    const dataPath = './src/server/data/users.json';
+
+    // helper methods
+    const readFile = (callback, returnJson = false, filePath = dataPath, encoding = 'utf8') => {
+        fs.readFile(filePath, encoding, (err, data) => {
+            if (err) {
+                throw err;
+            }
+
+            callback(returnJson ? JSON.parse(data) : data);
+        });
+    };
+
+    const writeFile = (fileData, callback, filePath = dataPath, encoding = 'utf8') => {
+
+        fs.writeFile(filePath, fileData, encoding, (err) => {
+            if (err) {
+                throw err;
+            }
+
+            callback();
+        });
+    };
+
+    // READ
+    app.get('/v1/users', (req, res) => {
+        fs.readFile(dataPath, 'utf8', (err, data) => {
+            if (err) {
+                throw err;
+            }
+
+            res.send(JSON.parse(data));
+        });
+    });
+   
+    // READ
+    app.get('/v1/users/:id', (req, res) => {
+        fs.readFile(dataPath, 'utf8', (err, data) => {
+            if (err) {
+                throw err;
+            }
+
+            res.send(JSON.parse(data));
+        });
+    });
+
+   
+
+    // CREATE
+    app.post('/v1/users', (req, res) => {
+
+	    fs.readFile(dataPath, 'utf8', (err, data) => {
+            if (err) {
+                throw err;
+            }
+
+            res.status(200).send(JSON.parse(data));
+        });         
+    });
+
+
+    
+};
+
+module.exports = getUserData;
